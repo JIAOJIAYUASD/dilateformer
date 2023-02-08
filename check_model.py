c26862dab55efc97767af83770b1e4d878ec9ac7
JIAOJIAYUASD <61881733+JIAOJIAYUASD@users.noreply.github.com>
2023-02-08 10:55:47 +0800

    Update check_model.py

--- check_model.py
+++ check_model.py
@@ -1,9 +1,9 @@
 import torch
 from fvcore.nn import FlopCountAnalysis
 from fvcore.nn import flop_count_table
-from models import uniformer_small
+from models import *
 
-model = uniformer_small()
+model = dilateformer_tiny()
 model.eval()
 flops = FlopCountAnalysis(model, torch.rand(1, 3, 224, 224))
 print(flop_count_table(flops))
